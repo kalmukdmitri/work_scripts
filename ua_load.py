76c37f7768bf53cdf8e117ee500ed8fe24f0aab2
kalmukdmitri <kalmukdmitri@gmail.com>
2023-03-13 17:14:45 +0400

    Add full loade

--- ua_load.py
+++ ua_load.py
@@ -22,6 +22,12 @@ import urllib
 import os
 import pandas
 import time
+import gspread
+import sys
+import datetime
+
+
+
 def date_pairs(date1, date2, step= 1):
     pairs= []
     while date2 >= date1:
@@ -31,12 +37,44 @@ def date_pairs(date1, date2, step= 1):
         pairs.append(pair)
     pairs.reverse()
     return pairs
+
 path = "/home/dmitri_kalmuk/rising-minutia-372107-3f00351690a6.json"
 # path = "rising-minutia-372107-3f00351690a6.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
 key_path =  path
 gbq_credential = service_account.Credentials.from_service_account_file(key_path,)
 q_schema = 'SELECT * FROM UA_BACKGROUND.INFORMATION_SCHEMA.TABLES;'
+
+path = "rising-minutia-372107-3f00351690a6.json"
+SCOPES = ['https://spreadsheets.google.com/feeds',
+          'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(path, SCOPES)
+gc = gspread.authorize(credentials)
+
+def sheet_ready(df_r):
+    for i in df_r:
+        df_r[i]= df_r[i].astype(str)
+    rows  = [list(df_r.columns)]
+    for i in df_r.itertuples():
+        ls=list(i)[1:]
+        rows.append(ls)
+    return rows
+
+def log_data(message):
+    pre_pand = {
+        'log':[str(message)],
+        'date': [str(datetime.datetime.today())]
+    }
+    sh = gc.open_by_key("1glBDf82lysijZyZ39FwbQjuJptFuSdafGXq7cqHEHQ4")
+    wk = sh.worksheet('Sheet1')
+    list_of_dicts = wk.get_all_records()
+    calls_g_c = pandas.DataFrame(list_of_dicts)
+    pre_pand = pandas.DataFrame(pre_pand)
+    pre_pand = pandas.concat([calls_g_c,pre_pand])
+    g_clop = sheet_ready(pre_pand)
+    wk.update('A1',g_clop)
+    return pre_pand
+
 
 
 tables  = {'sessions': {'dimetions': [
@@ -85,23 +123,31 @@ account = [
  ['connexy_com', '272663387', datetime.date(2022, 1, 1)]
 ]
 
+credentials = ServiceAccountCredentials.from_json_keyfile_name(path, SCOPES)
+gc = gspread.authorize(credentials)
+sh = gc.open_by_key("1glBDf82lysijZyZ39FwbQjuJptFuSdafGXq7cqHEHQ4")
 for acc in account:
     print(acc)
     for table in tables:
-        table_loaded = acc[0]+'_'+table
-        print(table_loaded)
-        bq_tables = pandas_gbq.read_gbq(q_schema, project_id='rising-minutia-372107', credentials=gbq_credential) 
-        start_date = acc[2]
-        if table_loaded in list(bq_tables['table_name']):
-            q_date = f'SELECT max(date) as first_dt FROM UA_BACKGROUND.{table_loaded};'
-            first_dt = pandas_gbq.read_gbq(q_date, project_id='rising-minutia-372107', credentials=gbq_credential)['first_dt'][0]
-            if first_dt:
-                start_date = datetime.datetime.strptime(first_dt,"%Y-%m-%d").date()  
-            
-        dates_to_load = date_pairs(start_date, datetime.datetime.today().date())
-        ga_conc = ga_connect(acc[1])
-        for dates in dates_to_load:
-            UA_report2 = ga_conc.report_pd([[dates[0],dates[1]]], tables[table])
-            UA_report2['date'] = dates[0]
-            UA_report2.to_gbq(f'UA_BACKGROUND.{table_loaded}', project_id='rising-minutia-372107',chunksize=20000, if_exists='append', credentials=gbq_credential)
-            time.sleep(3)
+        try:
+            table_loaded = acc[0]+'_'+table
+            print(table_loaded)
+            bq_tables = pandas_gbq.read_gbq(q_schema, project_id='rising-minutia-372107', credentials=gbq_credential) 
+            start_date = acc[2]
+            if table_loaded in list(bq_tables['table_name']):
+                q_date = f'SELECT max(date) as first_dt FROM UA_BACKGROUND.{table_loaded};'
+                first_dt = pandas_gbq.read_gbq(q_date, project_id='rising-minutia-372107', credentials=gbq_credential)['first_dt'][0]
+                if first_dt:
+                    start_date = datetime.datetime.strptime(first_dt,"%Y-%m-%d").date()  
+
+            dates_to_load = date_pairs(start_date, datetime.datetime.today().date())
+            ga_conc = ga_connect(acc[1])
+            for dates in dates_to_load:
+                UA_report2 = ga_conc.report_pd([[dates[0],dates[1]]], tables[table])
+                UA_report2['date'] = dates[0]
+                UA_report2.to_gbq(f'UA_BACKGROUND.{table_loaded}', project_id='rising-minutia-372107',chunksize=20000, if_exists='append', credentials=gbq_credential)
+                time.sleep(3)
+                mess = table_loaded + " - " + str(len(UA_report2))
+                log_data(mess)
+        except:
+            log_data(str(sys.exc_info()[1]))
